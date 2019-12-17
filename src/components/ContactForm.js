4f51022a92fda5b65c8b5218d7cdead77bdbc11c
David Salazar <david.asal@hotmail.com>
2019-12-17 12:57:03 -0600

    added contact from test 2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,12 +3,7 @@ import React from 'react';
 class ContactForm extends React.Component {
   render() {
     return (
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
+      <form name="contact" method="post">
         <input type="hidden" name="form-name" value="contact" />
         <div className="input-wrap">
           <input
